f77ac8c8b77e96172dd5e9b4c93e54313b289f17
Bajarang <bajarangjadhav2018@gmail.com>
2020-05-10 12:42:19 +0530

    Updates

---
+++ precache-manifest.3ce92dcf47271c911d34a8b60f909586.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73b8c598ebf37ebb4883c4836feb87cf",
+    "url": "/react-app-monsters-rolodex/index.html"
+  },
+  {
+    "revision": "afb02280aaa1ed47c545",
+    "url": "/react-app-monsters-rolodex/static/css/main.7e8759db.chunk.css"
+  },
+  {
+    "revision": "6166513946a491b652d8",
+    "url": "/react-app-monsters-rolodex/static/js/2.c86f1a05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app-monsters-rolodex/static/js/2.c86f1a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "afb02280aaa1ed47c545",
+    "url": "/react-app-monsters-rolodex/static/js/main.fe895e27.chunk.js"
+  },
+  {
+    "revision": "ea81b217ad810ea7b817",
+    "url": "/react-app-monsters-rolodex/static/js/runtime-main.e71dc444.js"
+  }
+]);
